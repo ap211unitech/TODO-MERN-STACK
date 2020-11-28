dfb62c706c01589a7a1cf77f9d478f8dc27f0883
AP <porwal.1@iitj.ac.in>
2020-11-28 12:31:26 +0530

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyparser = require("body-parser");
+const path = require("path");
 
 //Connect Database
 require("./db/connect")
@@ -17,9 +18,20 @@ app.use(bodyparser.urlencoded({ extended: true }));
 //Routes
 app.use("/items", require("./routes/api/items"))
 
+//Serve static assets if application in production
+if (process.env.NODE_ENV === "production") {
+    //Set Static Folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 //Server
 const port = process.env.PORT || 5000
+
 app.listen(port, () => {
     console.log(`Server started successfully on port ${port}`);
 })
